852d28b227b0515eefb4f23fcb77861930ebb663
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2025-09-05 08:38:22 +0800

    Delete attendance directory

--- attendance/core/classLoaders.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once "dbConfig.php";
-    require_once "../classes/admins.php";
-    require_once "../classes/students.php";
-
-    $obj_Databse = new Database();
-    // $obj_Admin = new Admin();
-    $obj_Student = new Student();
-?>
